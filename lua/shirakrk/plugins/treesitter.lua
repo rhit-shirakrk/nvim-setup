5b8ad5a1bfac208326bd24bb8888d56c99ad574f
Ryan Shiraki <shirakrk@rose-hulman.edu>
2024-05-30 20:45:28 -0400

    added treesitter

---
+++ lua/shirakrk/plugins/treesitter.lua
@@ -0,0 +1,44 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPre", "BufNewFile" },
+    build = ":TSUpdate",
+    dependencies = {
+        "windwp/nvim-ts-autotag",
+    },
+    config = function()
+        local treesitter = require("nvim-treesitter.configs")
+
+        treesitter.setup({
+            highlight = {
+                enable = true,
+            },
+            indent = { enable = true },
+            autotag = {
+                enable = true,
+            },
+            ensure_installed = {
+                "bash",
+                "c",
+                "comment",
+                "cpp",
+                "lua",
+                "markdown",
+                "markdown_inline",
+                "python",
+                "vim",
+                "vimdoc",
+                "xml",
+                "yaml",
+            },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<C-space>",
+                    node_incremental = "<C-space>",
+                    scope_incremental = false,
+                    node_decremental = "<bs>",
+                },
+            },
+        })
+    end,
+}
