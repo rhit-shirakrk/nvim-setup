a2d2d8b0cca5d8f408eea98d25c0b6ef0d3a790b
Ryan Shiraki <shirakrk@rose-hulman.edu>
2024-05-31 10:31:49 -0400

    added gitsigns

--- lua/shirakrk/plugins/linting.lua
+++ lua/shirakrk/plugins/linting.lua
@@ -13,7 +13,7 @@ return {
 			yaml = { "yamllint" },
 		}
 
-		local lint_augroup = vim.api.nvim_create_autogroup("lint", { clear = true })
+		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
 		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
 			group = lint_augroup,
