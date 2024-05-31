3f79e54fa524730e8af245b25dc39d512c539195
Ryan Shiraki <shirakrk@rose-hulman.edu>
2024-05-30 22:12:46 -0400

    added lsp functionality

--- lua/shirakrk/lazy.lua
+++ lua/shirakrk/lazy.lua
@@ -11,7 +11,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("shirakrk.plugins", {
+require("lazy").setup({ { import = "shirakrk.plugins" }, { import = "shirakrk.plugins.lsp" } }, {
     checker = {
         enabled = true,
         notify = false,
