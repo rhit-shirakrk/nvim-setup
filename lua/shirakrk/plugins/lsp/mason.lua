8d427be3989dda06d5cd30dcdf3c329eaa9add31
Ryan Shiraki <shirakrk@rose-hulman.edu>
2024-05-31 10:08:42 -0400

    added formatting

--- lua/shirakrk/plugins/lsp/mason.lua
+++ lua/shirakrk/plugins/lsp/mason.lua
@@ -1,33 +1,48 @@
 return {
-    "williamboman/mason.nvim",
-    dependencies = {
-        "williamboman/mason-lspconfig.nvim",
-    },
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
 
-    config = function()
-        local mason = require("mason")
-        local mason_lspconfig = require("mason-lspconfig")
+	config = function()
+		local mason = require("mason")
+		local mason_lspconfig = require("mason-lspconfig")
+		local mason_tool_installer = require("mason-tool-installer")
 
-        mason.setup({
-            ui = {
-                icons = {
-                    package_installed = "✓",
-                    package_pending = "➜",
-                    package_uninstalled = "✗",
-                },
-            },
-        })
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
 
-        mason_lspconfig.setup({
-            ensure_installed = {
-                "bashls",
-                "clangd",
-                "lua_ls",
-                "marksman",
-                "pyright",
-                "lemminx",
-                "yamlls",
-            },
-        })
-    end,
+		mason_lspconfig.setup({
+			ensure_installed = {
+				"bashls",
+				"clangd",
+				"lua_ls",
+				"marksman",
+				"pyright",
+				"lemminx",
+				"yamlls",
+			},
+		})
+
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"beautysh",
+				"clang-format",
+				"stylua",
+				"markdownlint",
+				"isort",
+				"black",
+				"xmlformatter",
+				"yamlfmt",
+			},
+		})
+	end,
 }
