a5b4ba236373b9adcc8b124fb43f727d2e8213b4
Ryan Shiraki <shirakrk@rose-hulman.edu>
2024-09-06 18:08:04 -0400

    updated day/night cycle

--- lua/shirakrk/plugins/colorscheme.lua
+++ lua/shirakrk/plugins/colorscheme.lua
@@ -2,8 +2,8 @@ return {
 	"folke/tokyonight.nvim",
 	priority = 1000,
 	config = function()
-		time = os.date("*t")
-		if time.hour >= 7 and time.hour <= 18 then
+		local time = os.date("*t")
+		if time.hour > 7 and time.hour < 18 then
 			vim.cmd("colorscheme tokyonight-day")
 		else
 			vim.cmd("colorscheme tokyonight-night")
