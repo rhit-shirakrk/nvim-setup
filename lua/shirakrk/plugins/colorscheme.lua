56445c38bb912eaebd41ce89ecee0530d9821630
Ryan Shiraki <shirakrk@rose-hulman.edu>
2024-09-06 13:58:20 -0400

    long needed update

--- lua/shirakrk/plugins/colorscheme.lua
+++ lua/shirakrk/plugins/colorscheme.lua
@@ -1,12 +1,12 @@
 return {
-    "folke/tokyonight.nvim",
-    priority = 1000,
-    config = function()
-        time = os.date("*t")
-        if time.hour >= 7 and time.hour <= 17 then
-            vim.cmd("colorscheme tokyonight-day")
-        else
-            vim.cmd("colorscheme tokyonight-night")
-        end
-    end,
+	"folke/tokyonight.nvim",
+	priority = 1000,
+	config = function()
+		time = os.date("*t")
+		if time.hour >= 7 and time.hour <= 18 then
+			vim.cmd("colorscheme tokyonight-day")
+		else
+			vim.cmd("colorscheme tokyonight-night")
+		end
+	end,
 }
